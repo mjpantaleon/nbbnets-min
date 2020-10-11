d8f52f70909e19982b99ebb2a6c6a6b311d82407
mj pantaleon <contact.mjpantaleon@gmail.com>
2020-10-12 02:43:11 +0800

    Remove migrations/2020_10_10_174045_add_igg_field_to_pre_screened_donors_table.php

---
+++ database/migrations/2020_10_12_023114_create_igg_results_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIggResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('igg_results', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('donation_id')->nullable();
+            $table->string('donor_sn', 16)->nullable();
+
+            $table->char('igg', 1)->nullable();                 /* N = NEGATIVE, P = POSITIVE */
+            $table->integer('cut_off_val', 5)->nullable();      /* PASSING VALUE 2+ */
+
+            $table->string('result_by', 50)->nullable();
+            $table->dateTime('result_dt')->nullable(); 
+
+            $table->string('approved_by', 50)->nullable();
+            $table->dateTime('approval_dt')->nullable(); 
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('igg_results');
+    }
+}
