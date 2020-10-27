a8f76a6e15d17e423665ffc4f596c414c16a56cd
mj-git <contact.mjpantaleon@gmail.com>
2020-10-27 18:07:00 +0800

    Done HNA & HNL test module

--- app/Http/Controllers/AdditionalTestController.php
+++ app/Http/Controllers/AdditionalTestController.php
@@ -20,7 +20,8 @@ class AdditionalTestController extends Controller
     LEFT JOIN additionaltest ad ON c.donation_id = ad.donation_id
     WHERE c.created_dt BETWEEN '2020-07-01' AND '2020-10-17'
     AND c.location = '13109'
-    AND c.comp_stat = 'FBT' 
+    AND c.comp_stat = 'FBT'
+    AND rc.component_abbr IS NOT NULL 
     AND ad.nat IS NULL AND ad.nat_by IS NULL
     */
     public function getComponentsForNatTest(Request $request){
@@ -35,7 +36,8 @@ class AdditionalTestController extends Controller
                 LEFT JOIN additionaltest ad ON c.donation_id = ad.donation_id
                 WHERE c.created_dt BETWEEN '$from' AND '$to'
                 AND c.location = '$facility_cd'
-                AND c.comp_stat = 'FBT' 
+                AND c.comp_stat = 'FBT'
+                AND rc.component_abbr IS NOT NULL 
                 AND ad.nat IS NULL AND ad.nat_by IS NULL ";
 
         $nat = DB::select($sql);
@@ -130,7 +132,8 @@ class AdditionalTestController extends Controller
     LEFT JOIN additionaltest ad ON c.donation_id = ad.donation_id
     WHERE c.created_dt BETWEEN '2020-07-01' AND '2020-10-17'
     AND c.location = '13109'
-    AND c.comp_stat = 'FBT' 
+    AND c.comp_stat = 'FBT'
+    AND rc.component_abbr IS NOT NULL 
     AND ad.zika IS NULL AND ad.zika_by IS NULL
     */
     public function getComponentsForZikaTest(Request $request){
@@ -145,7 +148,8 @@ class AdditionalTestController extends Controller
                 LEFT JOIN additionaltest ad ON c.donation_id = ad.donation_id
                 WHERE c.created_dt BETWEEN '$from' AND '$to'
                 AND c.location = '$facility_cd'
-                AND c.comp_stat = 'FBT' 
+                AND c.comp_stat = 'FBT'
+                AND rc.component_abbr IS NOT NULL 
                 AND ad.zika IS NULL AND ad.zika_by IS NULL ";
 
         $zika = DB::select($sql);
@@ -239,12 +243,123 @@ class AdditionalTestController extends Controller
     SELECT c.donation_id, c.component_cd, rc.component_abbr
     FROM component c
     LEFT JOIN r_cp_component_codes rc ON c.component_cd = rc.component_code
-    LEFT JOIN additional_hna_tests ad ON c.donation_id = ad.donation_id
+    LEFT JOIN additional_hna_tests ad ON BINARY c.donation_id = BINARY ad.donation_id
     WHERE c.created_dt BETWEEN '2020-07-01' AND '2020-10-17'
-    AND c.location = '13109'
-    AND c.comp_stat = 'FBT' 
+    AND c.location = '11001'
+    AND c.comp_stat = 'FBT'
+    AND rc.component_abbr IS NOT NULL 
     AND ad.hna_hnl_result IS NULL AND ad.result_by IS NULL
     */
+    public function getComponentsForHnaTest(Request $request){
+        $ids            = [];
+        $from           = date($request['date_from']);
+        $to             = date($request['date_to']);
+        $facility_cd    = Session::get('userInfo')['facility']['facility_cd'];
+        
+        $sql =" SELECT c.donation_id, c.component_cd, rc.component_abbr
+                FROM component c
+                LEFT JOIN r_cp_component_codes rc ON c.component_cd = rc.component_code
+                LEFT JOIN additional_hna_tests ad ON BINARY c.donation_id = BINARY ad.donation_id
+                WHERE c.created_dt BETWEEN '$from' AND '$to'
+                AND c.location = '$facility_cd'
+                AND c.comp_stat = 'FBT' 
+                AND rc.component_abbr IS NOT NULL
+                AND ad.hna_hnl_result IS NULL AND ad.result_by IS NULL ";
 
+        $hna_hnl = DB::select($sql);
+
+        \Log::info($hna_hnl);
+        
+        $hna_hnl = json_decode(json_encode($hna_hnl), true);
+        // return($hna_hnl);
+        
+        if($hna_hnl){
+            for($i = 0; $i < count($hna_hnl); $i++){
+                $ids[$i]['donation_id'] = $hna_hnl[$i]['donation_id'];
+                $ids[$i]['component_abbr'] = $hna_hnl[$i]['component_abbr'];
+                $ids[$i]['hna_hnl_result'] = "";
+            }
+            
+            \Log::info($ids);
+            return $ids;
+        } else {
+            return false;
+        }
+    } /* getComponentsForHnaTest() */
+
+
+    public function saveHnaResult(Request $request){
+        $facility_user  = Session::get('userInfo')['user_id'];
+        $facility_cd    = Session::get('userInfo')['facility']['facility_cd'];
+
+        $verifier       = $request->get('verifier');
+        
+        $hna_hnl_results  = $request->get('hna_hnl_results');
+        \Log::info($hna_hnl_results);
+        // return $hna_hnl_results;
+
+        $duplicated_id = '';
+
+        // EXTRACT DATA
+        foreach($hna_hnl_results as $h => $hn){
+            
+            // FOR NON EMPTY FIELDS
+            if($hn['hna_hnl_result']){
+                
+                $donation_id = $hn['donation_id'];
+                $hna_hnl_result = $hn['hna_hnl_result'];
+                
+                // CHECK IF DONATION ID ALREADY EXIST AT ADDITIONAL TEST TABLE
+                $check_donation_id = AdditionalHnaTest::where('donation_id', '=', $donation_id)->first();
+                
+                // IF DONATION ID DOESNT EXIST THEN
+                if($check_donation_id === null){
+                    
+                    // INSERT IN THE `additional_hna_test` TABLE
+                    $h = new AdditionalHnaTest;
+                    $h->donation_id = $donation_id;
+                    $h->hna_hnl_result = $hna_hnl_result;
+                    $h->result_by = $facility_user;
+                    $h->result_dt = date("Y-m-d H:i:s");
+                    $h->approved_by = $verifier;
+                    $h->approval_dt = date("Y-m-d H:i:s");
+                    $h->save();
+                    
+                }
+                
+                //  ELSE IF EXISTS
+                else{
+                    // concut/ list down donation ids
+                    $duplicated_id = $duplicated_id.' '.$bt['donation_id'];
+                }
+                
+                // RETURN RESPONSE
+            }
+        }
+        // EXTRACT DATA
+
+        \Log::info($duplicated_id);
+        // LOOP EACH RECORD SENT FROM TTI RESULT VUE
+
+        // if donation id already exist then 
+        if($duplicated_id){
+            
+            // send this response
+            return response()->json([
+                'message' => "This donation IDs already exist: \n $duplicated_id",
+                'status' => 1
+            ], 200);
+        }
+
+        // else proceed to save record
+        else{
+            return response()->json([
+                'message' => 'HNA & HNL result/s has been saved.',
+                'status' => 1
+            ], 200);
+        }
+    } /* saveHnaResult() */
+
+    
     // ///////////////////////////////////////////////// HNA & HNL ///////////////////////////////////
 }
