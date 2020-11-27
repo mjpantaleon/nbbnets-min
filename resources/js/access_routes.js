3d7d0348b5d4c3cb21b770719b3684e41a5343fb
mj-git <contact.mjpantaleon@gmail.com>
2020-11-27 15:16:27 +0800

    Added access_routes.js

---
+++ resources/js/access_routes.js
@@ -0,0 +1,35 @@
+export const ac_routes = [
+
+    { BCM : {
+        'pre-screened-list',
+        'pre-screened-info' ,
+        'pre-screening',
+        'pre-screening-igg',
+        'hla-hna-test',
+        'donation',
+        'new-walk-in',
+        'search-donor',
+        'donor-details',
+    }},
+
+    { MT = [
+        'blood-processing',
+        'labelling',
+        'inventory-release',
+        'available-stocks',
+        'units',
+        'blood-request-list',
+        'blood-request-create',
+        'blood-request-view',
+        'blood-request-issue',
+        'blood-request-print',
+    ]},
+
+    HBBM = [
+        'blood-typing',
+        'blood-test-tti',
+        'blood-test-nat',
+        'blood-test-zika',
+    ],
+
+];
