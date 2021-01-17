ebec6267ef4894412c78ca2b3c12c41039c50b95
mj pantaleon <contact.mjpantaleon@gmail.com>
2021-01-17 17:45:38 +0800

    DATE/TIME Issue

--- app/Http/Controllers/PreScreenedDonorController.php
+++ app/Http/Controllers/PreScreenedDonorController.php
@@ -318,6 +318,8 @@ class PreScreenedDonorController extends Controller
         $created_by = $facility_user;
         $created_dt = date('Y-m-d H:i:s');
 
+        \Log::info($created_dt);
+
         // check if record exists before inserting new record
         $check_existing_record = PreScreenedDonor::where('first_name', '=', $fname)
                                 ->where('middle_name', '=', $mname)
@@ -475,7 +477,8 @@ class PreScreenedDonorController extends Controller
                         $igg->approved_by = $verifier;
                         $igg->approval_dt = date("Y-m-d H:i:s");
                         $igg->save();
-
+                        
+                        \Log::info($igg->result_dt = date("Y-m-d H:i:s"));
                         // ----------------- FIX FOR REPEAT DONATION ON PRE-SCREENING
                         // ----------------- UPDATE `pre_screened_donors` table
                         PreScreenedDonor::where('donor_sn', $donor_sn)
